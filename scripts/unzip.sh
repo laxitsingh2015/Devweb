6a2750a4532df47ac9937e80d0fb274cb2bee5cc
Laxit <Laxit@gmail.com>
2024-09-05 11:16:02 +0530

    Without adding files in zip

--- scripts/unzip.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Unzip the file to the desired directory
-unzip -o /var/www/html/Web-Application.zip -d /var/www/html/
-
-# Optionally, remove the .zip file after extraction
-rm /var/www/html/Web-Application.zip
